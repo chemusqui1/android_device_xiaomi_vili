ca3f217d31808afb8fb64a5a031ffe70eac8c9e4
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-09 18:33:34 +0100

    vili: Initial device tree

---
+++ BoardConfig.mk
@@ -0,0 +1,10 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+DEVICE_PATH := device/xiaomi/vili
+
+# Board
+TARGET_BOOTLOADER_BOARD_NAME := vili
---
+++ lineage_vili.mk
@@ -0,0 +1,23 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from vili device
+$(call inherit-product, device/xiaomi/vili/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := vili
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := 2107113SG
+PRODUCT_NAME := lineage_vili
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
