00ea1dd5cc27605d64564e51262e0620322d82f1
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-09 20:19:03 +0100

    vili: Use stock FP and description
    * Safetynet is safetynet

--- lineage_vili.mk
+++ lineage_vili.mk
@@ -20,4 +20,10 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := 2107113SG
 PRODUCT_NAME := lineage_vili
 
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="vili_global-user 12 SKQ1.211001.001 V13.0.7.0.SKDMIXM release-keys"
+
+BUILD_FINGERPRINT := Xiaomi/vili_global/vili:12/SKQ1.211001.001/V13.0.7.0.SKDMIXM:user/release-keys
+
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
