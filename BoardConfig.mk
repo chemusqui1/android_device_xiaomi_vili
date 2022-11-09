5cdbaa773fb7d02e642a244f37bb4ed87e59fe32
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-09 18:41:05 +0100

    vili: Include the proprietary vendor tree

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,3 +11,6 @@ include device/xiaomi/sm8350-common/BoardConfigCommon.mk
 
 # Board
 TARGET_BOOTLOADER_BOARD_NAME := vili
+
+# Include proprietary-files
+include vendor/xiaomi/vili/BoardConfigVendor.mk
