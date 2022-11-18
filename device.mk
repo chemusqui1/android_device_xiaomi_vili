93a89fc3bc80c736412395932885a27c45348afc
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-18 16:31:35 +0100

    vili: Add an RRO for showing correct tethering SSID name
    * If not built, it will show generic Android_APxxxx, a bit ugly.
    It should be better to show device name as-is

--- device.mk
+++ device.mk
@@ -22,6 +22,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# RRO Overlays
+PRODUCT_PACKAGES += \
+    ViliWifiOverlay
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
