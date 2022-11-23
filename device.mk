6c30a7389ca13c785db2d6534ef6b0d63918ef50
chemusqui1 <61394758+chemusqui1@users.noreply.github.com>
2022-11-23 12:00:48 +0100

    vili: Add vendor public libraries
    * From stock, needed for some apps to work, like MemeGallery port

--- device.mk
+++ device.mk
@@ -26,6 +26,10 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+# Public Libraries
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt \
+
 # RRO Overlays
 PRODUCT_PACKAGES += \
     ViliWifiOverlay
